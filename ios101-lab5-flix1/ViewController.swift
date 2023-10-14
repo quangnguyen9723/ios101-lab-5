ae02beb9c0f119b213c479dfacde22ab8cd356f1
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-10-14 14:50:02 -0400

    finish lab

--- ios101-lab5-flix1/ViewController.swift
+++ ios101-lab5-flix1/ViewController.swift
@@ -7,20 +7,58 @@ import UIKit
 import Nuke
 
 // TODO: Add table view data source conformance
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("🍏 cellForRowAt called for row: \(indexPath.row)")
+
+            // Get a reusable cell
+            // Returns a reusable table-view cell object for the specified reuse identifier and adds it to the table. This helps optimize table view performance as the app only needs to create enough cells to fill the screen and reuse cells that scroll off the screen instead of creating new ones.
+            // The identifier references the identifier you set for the cell previously in the storyboard.
+            // The `dequeueReusableCell` method returns a regular `UITableViewCell`, so we must cast it as our custom cell (i.e., `as! MovieCell`) to access the custom properties you added to the cell.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+
+            // Get the movie associated table view row
+            let movie = movies[indexPath.row]
+
+            // Configure the cell (i.e., update UI elements like labels, image views, etc.)
+
+            // Unwrap the optional poster path
+            if let posterPath = movie.poster_path,
+
+                // Create a url by appending the poster path to the base url. https://developers.themoviedb.org/3/getting-started/images
+               let imageUrl = URL(string: "https://image.tmdb.org/t/p/w500" + posterPath) {
+
+                // Use the Nuke library's load image function to (async) fetch and load the image from the image URL.
+                Nuke.loadImage(with: imageUrl, into: cell.posterImageView)
+            }
+
+            // Set the text on the labels
+            cell.titleLabel.text = movie.title
+            cell.overviewLabel.text = movie.overview
+
+            // Return the cell for use in the respective table view row
+            return cell
+    }
+    
 
 
     // TODO: Add table view outlet
-
+    @IBOutlet weak var tableView: UITableView!
+    
 
     // TODO: Add property to store fetched movies array
+    private var movies: [Movie] = []
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // TODO: Assign table view data source
-
+        tableView.dataSource = self
 
         fetchMovies()
     }
@@ -79,8 +117,8 @@ class ViewController: UIViewController {
                     }
 
                     // TODO: Store movies in the `movies` property on the view controller
-
-
+                    self?.movies = movies
+                    self?.tableView.reloadData()
 
                 }
             } catch {
